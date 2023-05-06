4789b89c3d719f3a0437feaf524dafee0ac42a0b
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-05-05 19:00:10 -0500

    Creación index empresa - Avance

--- xampp/htdocs/app_web_php-main-AutOsiris/views/roles/admin/footer.view.php
+++ xampp/htdocs/app_web_php-main-AutOsiris/views/roles/admin/footer.view.php
@@ -1,3 +1,3 @@
-<div class="pie_de_pagina">
-    <h2>Pie de pagina</h2>
-</div>
+<footer>
+    
+</footer>
--- xampp/htdocs/app_web_php-main-AutOsiris/views/roles/admin/header.view.php
+++ xampp/htdocs/app_web_php-main-AutOsiris/views/roles/admin/header.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicio</title>
-    <link rel="stylesheet" href="assets/css/index.css">
+    <title>Papelería Osiris</title>
+    <link rel="stylesheet" href="assets/css/stylesEmpresa.css">
+    <link rel="shortcut icon" href="assets/img/papeleria_icono.png" type="image/x-icon">
 </head>
-
