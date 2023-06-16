138209d7f780ee0e9e4beed193656ce705f07c3c
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-06-15 21:39:50 -0500

    Corrección Interfaz de Administrador - PTPO no funcional

---
+++ docs/trim1/prototipo_no_funcional/Interfaces/Interfaz Administrador/assets/js/6. Facturas/Facturas.js
@@ -0,0 +1,3 @@
+function FacturaEliminada(){
+    alert("La Factura seleccionada ha sido eliminada");
+}
---
+++ docs/trim1/prototipo_no_funcional/Interfaces/Interfaz Administrador/assets/js/6. Facturas/Nueva_factura.js
@@ -0,0 +1,57 @@
+// Agregar Fila Factura
+// var contadorFilas = 1;
+
+function agregarFilaFactura() {
+    var tabla = document.getElementById("tabla-factura");
+    var filasDatos = tabla.insertRow(tabla.rows.length - 1);
+
+    // TR de Filas datos
+    filasDatos.className = "fila-datos";
+
+    // Nombre producto   
+    var nombreProducto = filasDatos.insertCell(0);
+    nombreProducto.className = "nombre-prod";
+    nombreProducto.innerHTML = "<td class='nombre-prod-dato'><input type='text' name='nombre-prod-input' id='nombre-prod-input' placeholder='Escribe aquí nombre de producto'></td>";
+
+    // Cantidad producto
+    var cantidadProducto = filasDatos.insertCell(1);
+    cantidadProducto.className = "cantidad-dato";
+    cantidadProducto.innerHTML = "<td class='cantidad-dato'><input type='number' name='cantidad-prod-input' id='cantidad-prod-input' placeholder='#'></td>";
+
+    // Precio unidad
+    var precioUnidad = filasDatos.insertCell(2);
+    precioUnidad.className = "precio-unidad-dato";
+    precioUnidad.innerHTML = "<td class='precio-unidad-dato'><input type='text' name='precio-unidad-input' id='precio-unidad-input' placeholder='$'></td>";
+
+    // Precio total
+    var precioTotal = filasDatos.insertCell(3);
+    precioTotal.className = "precio-total-dato";
+    precioTotal.innerHTML = "<td class='precio-total-dato'><input type='text' name='precio-total-input' id='precio-total-input' placeholder='$$$'></td>";
+
+
+    var accion = filasDatos.insertCell(4);
+    accion.className = "elim-fila";
+    var botonEliminar = document.createElement("button");
+    botonEliminar.innerHTML = "<td class='elim-fila'><img src='../../../assets/img/icono-eliminar-rojo.png'></td>";
+    botonEliminar.onclick = function() {
+        eliminarFila(this);
+    };
+    
+    accion.appendChild(botonEliminar);
+}
+
+function eliminarFila(boton) {
+  var filasDatos = boton.parentNode.parentNode;
+  filasDatos.parentNode.removeChild(filasDatos);
+}
+
+// Eliminar Fila
+function eliminarFilaFactura(boton) {
+  var filasDatos = boton.parentNode.parentNode;
+  filasDatos.parentNode.removeChild(filasDatos);
+}
+
+// Cambios guardados
+function FacturaCreada(){
+    alert("La Factura ha sido creada");
+}
